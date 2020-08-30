43d8145d08e8ffc6319ee5a410de1792392c703a
Siddharth Prakashan <prakashansiddharth@gmail.com>
2020-08-30 21:11:28 +0530

    added auto update datetime to sql db

--- ajax/updateDuration.php
+++ ajax/updateDuration.php
@@ -2,8 +2,9 @@
 require_once("../includes/config.php");
 
 if(isset($_POST["videoId"]) && isset($_POST["username"]) && isset($_POST["progress"])) {
-    $query = $con->prepare("UPDATE videoProgress SET progress=:progress,
-                            dateModified=NOW() WHERE username=:username AND videoId=:videoId");
+    $query = $con->prepare("UPDATE videoProgress 
+                            SET progress=:progress 
+                            WHERE username=:username AND videoId=:videoId");
     $query->bindValue(":username", $_POST["username"]);
     $query->bindValue(":videoId", $_POST["videoId"]);
     $query->bindValue(":progress", $_POST["progress"]);
