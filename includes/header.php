1469d688a6ef77baa21a679d19359cab949baaa0
Siddharth Prakashan <prakashansiddharth@gmail.com>
2020-08-30 20:59:31 +0530

    done

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<?php
+require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
+require_once("includes/classes/CategoryContainers.php");
+require_once("includes/classes/Entity.php");
+require_once("includes/classes/EntityProvider.php");
+require_once("includes/classes/ErrorMessage.php");
+require_once("includes/classes/SeasonProvider.php");
+require_once("includes/classes/Season.php");
+require_once("includes/classes/Video.php");
+require_once("includes/classes/VideoProvider.php");
+require_once("includes/classes/User.php");
+
+if(!isset($_SESSION["userLoggedIn"])) {
+    header("Location: register.php");
+}
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to SIDFLIX</title>
+
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+      integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://kit.fontawesome.com/2935413df3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/style/style.css">
+    <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+        <div class='wrapper'>
+<?php
+if(!isset($hideNav)) {
+    include_once("includes/navBar.php");
+}
+?>
