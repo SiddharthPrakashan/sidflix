79a296d24bca4d90b93a7520094a7fd9dda6622d
Siddharth Prakashan <prakashansiddharth@gmail.com>
2020-09-02 11:28:28 +0530

    changes

--- search.php
+++ search.php
@@ -4,22 +4,15 @@ include_once("includes/header.php");
 <div class="textboxContainer">
     <input type="text" class="searchInput" placeholder="Search for something">
 </div>
-
 <div class="results"></div>
-
 <script>
-
 $(function() {
-
     var username = '<?php echo $userLoggedIn; ?>';
     var timer;
-
     $(".searchInput").keyup(function() {
         clearTimeout(timer);
-
         timer = setTimeout(function() {
-            var val = $(".searchInput").val();
-            
+            var val = $(".searchInput").val();      
             if(val != "") {
                 $.post("ajax/getSearchResults.php", { term: val, username: username }, function(data) {
                     $(".results").html(data);
@@ -28,10 +21,7 @@ $(function() {
             else {
                 $(".results").html("");
             }
-
         }, 500);
     })
-
 })
-
 </script>
